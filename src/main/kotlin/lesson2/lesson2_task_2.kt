5bab3b2b2fb7a3c6a54eca16d8315120746bfc8d
a.ganieva <a.ganieva@vk.team>
2023-09-04 08:28:03 +0400

    add lessons 2

---
+++ src/main/kotlin/lesson2/lesson2_task_2.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val  sumOfOrder: Int = 50
+
+    val notificationText: String = "Спасибо за заказ!"
+
+ //   println(sumOfOrders)
+//    println(notificationText)
+
+  //  println(sumOfOrder::class.simpleName)
+  //  println(notificationText::class.simpleName)
+
+    val customerTotal = 2000
+//   println(customerTotal)
+
+    var customerOff = 2
+
+    var customerAfterOff = customerTotal - customerOff
+
+    println(customerAfterOff)
+
+
+}

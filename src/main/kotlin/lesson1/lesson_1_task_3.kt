9425b46fda167cb3521d05934cf18a95114551e6
a.ganieva <a.ganieva@vk.team>
2023-09-04 13:00:08 +0400

    add lessons 2

---
+++ src/main/kotlin/lesson1/lesson_1_task_3.kt
@@ -0,0 +1,24 @@
+fun main() {
+   val year = 1961
+   var hour = "09"
+    var minute = ":07"
+
+
+    //var multiString= """
+   // 09
+    //07
+    //""".trimIndent()
+
+
+    println(year)
+    //println(multiString)
+
+    println("$hour$minute")
+
+
+
+}
+
+
+
+

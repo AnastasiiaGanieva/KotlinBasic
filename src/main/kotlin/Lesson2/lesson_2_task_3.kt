8b0b4bd15d8742385855e5ca08af70673bf02e18
a.ganieva <a.ganieva@vk.team>
2023-09-22 19:05:52 +0400

    lesson2

---
+++ src/main/kotlin/Lesson2/lesson_2_task_3.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val timeOfLeavingHour = 9
+    val timeOfLeavingMinute = 45
+
+    val timeInRoadHour = 8
+    val timeInRoadMinute = 1
+
+    val arrivalHour = timeOfLeavingHour + timeInRoadHour
+    val arrivalMinute = timeOfLeavingMinute + timeInRoadMinute
+    val notes = ":"
+
+
+
+ println("$arrivalHour$notes$arrivalMinute")
+
+}
+
+

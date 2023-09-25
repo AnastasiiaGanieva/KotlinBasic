7d238e0559adca7e03248380e4ccb9fb6e6b911c
a.ganieva <a.ganieva@vk.team>
2023-09-25 10:36:11 +0400

    lesson2

---
+++ src/main/kotlin/Lesson2/lesson_2_task_4.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val cristal: Int = 7
+    val iron: Int = 11
+    val buff=20
+    val buffOfCristal= (cristal*buff)/100
+    val buffOfIron = (iron*buff)/100
+
+    val buffCristalTotal=cristal+buffOfCristal
+    val buffIronTotal=iron+buffOfIron
+
+    println(buffCristalTotal)
+    println(buffIronTotal)
+}
+

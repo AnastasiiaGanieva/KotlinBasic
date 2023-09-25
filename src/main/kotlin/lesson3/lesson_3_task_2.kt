d22cc15d0470491b84baa945874f43f63d4e7f4e
a.ganieva <a.ganieva@vk.team>
2023-09-25 17:07:51 +0400

    lesson2

--- src/main/kotlin/lesson3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson3/lesson_3_task_2.kt
@@ -1,2 +1,11 @@
-package lesson3
+fun main() {
+    val name = "Татьяна"
+    val surnameBeforeMarried = "Андреева"
+    val patronymic = "Сергеевна"
+    val yearBeforeMarried = 20
+    val yearAfterMarried= 22
+    val surnameAfterMarried = "Сидорова"
 
+    println("$surnameBeforeMarried $name $patronymic,$yearBeforeMarried лет")
+    println("$surnameAfterMarried $name $patronymic,$yearAfterMarried года ")
+}
---
+++ src/main/kotlin/lesson3/lesson_3_task_3.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var number = 6
+    var counter = 1
+
+    while (counter < 10) {
+        var total = number*counter
+        println("$number * $counter = $total")
+        counter++
+    }
+}

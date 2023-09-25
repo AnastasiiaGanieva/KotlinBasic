5fd7aebca9a891e3cac824c978a832f69f07e624
a.ganieva <a.ganieva@vk.team>
2023-09-25 11:06:11 +0400

    lesson2

---
+++ src/main/kotlin/lesson3/lesson_3_task_1.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val morningGreetings = "Доброе утро"
+    val nightGreetings= "Доброй ночи"
+    val userName = "Владислав"
+
+    println("$morningGreetings,$userName!")
+    println("$nightGreetings,$userName!")
+
+
+
+}
